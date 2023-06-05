9d120969f0c2ce87866ca0050c458be319a05c3e
Santiago1809 <aristizabalsantiago482@gmail.com>
2023-06-05 10:04:47 -0500

    Hover a los cards de los grupos

--- client/src/components/index/IndexCardComponent.vue
+++ client/src/components/index/IndexCardComponent.vue
@@ -4,7 +4,7 @@
   >
     <div class="px-6 py-4 flex justify-between items-center">
       <div class="font-bold text-xl mb-2">
-        <router-link :to="ruta">{{ nombreGrupo }}</router-link>
+        <router-link :to="ruta" class="hover:underline hover:text-blue-700">{{ nombreGrupo }}</router-link>
       </div>
       <div class="flex">
         <ModalComponent
