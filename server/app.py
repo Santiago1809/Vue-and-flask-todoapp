a6c87e1f2ad815f254022d5581671c227a4267d1
Santiago1809 <96321520+Santiago1809@users.noreply.github.com>
2023-11-23 08:53:31 -0500

    Update app.py

--- server/app.py
+++ server/app.py
@@ -7,9 +7,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = "Secret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/todo_app"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://santiago:ryh89vw3Nr35Xru1l1kn5hL5FzsER7JI@dpg-clfldtvjc5ks73e88a00-a/vuenotes"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
